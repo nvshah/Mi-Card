9f2aefb6443968f5dcbf66b833c6350e05a2af7c
nvshah <nipunshah6776@gmail.com>
2020-11-08 15:34:58 +0530

    Column - (Widgets)

--- lib/main.dart
+++ lib/main.dart
@@ -15,9 +15,27 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(
-            color: Colors.white,
-            child: Text('Hola All'),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                color: Colors.white,
+                child: Text('Container 1'),
+                height: 100.0,
+              ),
+              SizedBox(height: 20.0,),
+              Container(
+                color: Colors.red,
+                child: Text('Container 2'),
+                height: 100.0,
+              ),
+              SizedBox(height: 20.0,),
+              Container(
+                color: Colors.green,
+                child: Text('Container 3'),
+                height: 100.0,
+              ),
+            ],
           ),
         ),
       ),
