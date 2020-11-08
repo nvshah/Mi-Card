ebd4a012db0ddbd195edc48ffec8d88d9a87a6bb
nvshah <nipunshah6776@gmail.com>
2020-11-08 17:13:10 +0530

    Basic My Info

--- lib/main.dart
+++ lib/main.dart
@@ -18,22 +18,81 @@ class MyApp extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
+              //Image
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/nipun.jpg'),
+              ),
+              //Name
+              Text(
+                'Nipun Shah',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 36.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              //About
+              Text(
+                'Passionate Developer',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.teal.shade100,
+                  letterSpacing: 2.5,
+                ),
+              ),
+              //Phone
               Container(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                padding: EdgeInsets.all(10.0),
                 color: Colors.white,
-                child: Text('Container 1'),
-                height: 100.0,
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Text(
+                      '+91-7046853655',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontSize: 20.0,
+                        fontFamily: 'Source Sans Pro',
+                      ),
+                    )
+                  ],
+                ),
               ),
-              SizedBox(height: 20.0,),
+              //Email
               Container(
-                color: Colors.red,
-                child: Text('Container 2'),
-                height: 100.0,
-              ),
-              SizedBox(height: 20.0,),
-              Container(
-                color: Colors.green,
-                child: Text('Container 3'),
-                height: 100.0,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                padding: EdgeInsets.all(10.0),
+                color: Colors.white,
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Text(
+                      'nipunshah6776@gmail.com',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontSize: 20.0,
+                        fontFamily: 'Source Sans Pro',
+                      ),
+                    )
+                  ],
+                ),
               ),
             ],
           ),
